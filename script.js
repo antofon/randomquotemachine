3db0629f751d12d856df09b7ade4a3b08888ff98
Anwana Ntofon <aontofon@ucdavis.edu>
2017-11-15 12:40:31 -0800

    Only whitespace changes

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ $(document).ready(function() {
   console.log("JavaScript loaded.");
   "use strict";
 
+
+  $.ajax({url: 'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&jsonp=displayQuote&lang=en', type: 'GET', dataType: 'jsonp'})
+
+
   $("#getQuote").click(function() {
-    //perform AJAX request, configure by passing through key-value pairs 
+    //perform AJAX request, configure by passing through key-value pairs
     $.ajax({url: 'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&jsonp=displayQuote&lang=en', type: 'GET', dataType: 'jsonp'})
   });
 });
