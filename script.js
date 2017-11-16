6dd87f4b9bd38c68c1e73fbc0ca47f8e216860eb
Anwana Ntofon <aontofon@ucdavis.edu>
2017-11-16 14:37:27 -0800

    Only whitespace changes

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ $(document).ready(function() {
   console.log("JavaScript loaded.");
   "use strict";
 
-
   $.ajax({url: 'https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&jsonp=displayQuote&lang=en', type: 'GET', dataType: 'jsonp'})
 
-
   $("#getQuote").click(function() {
+
     //perform AJAX request, configure by passing through key-value pairs
     $.ajax({url: 'https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&jsonp=displayQuote&lang=en', type: 'GET', dataType: 'jsonp'})
   });
